e6540b8b0b52b8d005cd31f45060f2c19546e1f3
skypatrol725 <sekunekiltnodi@gmail.com>
2025-11-19 10:33:47 +0100

    test

---
+++ open_file.c
@@ -0,0 +1,5 @@
+#include "proto.h"
+
+/*This function is going to read a map file in parameter and to fill a structure with the map.
+It returns 0 if */
+int	fill_my_struct()
---
+++ proto.h
@@ -0,0 +1,12 @@
+#ifndef PROTO_H
+#define PROTO_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// typedef struct {
+// 	int 
+// }
+
+#endif //PROTO_H
