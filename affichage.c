d1f6fcbd88cbcb0dd46fd816f5243febb02b11ca
skypatrol725 <sekunekiltnodi@gmail.com>
2026-01-07 09:29:14 +0100

    few changes ;)

--- affichage.c
+++ affichage.c
@@ -80,35 +80,16 @@ void afficherGrilleNcurses(const Grille *g, int x, int y)
     }
 }
 
-
-// void afficherGrilleNcurses(const Grille *g, int x, int y)
-// {
-//     for (int i = 0; i < g->taille; i++)
-//     {
-//         for (int j = 0; j < g->taille; j++)
-//         {
-//             int region = g->regions[i][j];
-
-//             /* sécurité */
-//             if (region < 0) region = 0;
-//             if (region > 7) region = 7;
-
-//             int pair = 1 + region;
-//             int is_cursor = (i == x && j == y);
-
-//             attron(COLOR_PAIR(pair));
-//             if (is_cursor)
-//                 attron(A_REVERSE);
-
-//             mvprintw(i, j * 3, "   ");
-
-//             if (g->etat[i][j] == QUEEN)
-// 				mvaddch(i, j * 3 + 1, 'Q');
-// 			else if (g->etat[i][j] == MARK)
-// 				mvaddch(i, j * 3 + 1, 'X');
-//             if (is_cursor)
-//                 attroff(A_REVERSE);
-//             attroff(COLOR_PAIR(pair));
-//         }
-//     }
-// }
+void afficherGrilleBlanche(const Grille *g)
+{
+    /* Masque la grille : toutes les cases en blanc, sans Q/X, sans couleur de region */
+    for (int i = 0; i < g->taille; i++)
+    {
+        for (int j = 0; j < g->taille; j++)
+        {
+            attron(COLOR_PAIR(8)); /* noir sur blanc */
+            mvprintw(i, j * 3, "   ");
+            attroff(COLOR_PAIR(8));
+        }
+    }
+}
